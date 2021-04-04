c6c89c6bf4cb9c3e5f0a98ea697fd9422e8b7a49
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-04-04 23:31:18 +0900

    📝textModal

---
+++ src/components/atoms/ChakraTheme/color.tsx
@@ -0,0 +1,21 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    color: {
+      100: "#76FADD",
+      200: "#F6CE49",
+      300: "#F266FC",
+      400: "#59bda7",
+      500: "#bda75e",
+      600: "#b75fbd",
+      700: "#fff",
+      800: "#3e3e3e",
+      900: "rgba(0, 0, 0, 0.6)",
+      1000: "#324A5E",
+      1100: "#DA1725",
+      1200: "#8a8a8a",
+      1300: "#a9141e",
+    },
+  },
+});
