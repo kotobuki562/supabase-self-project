5a961824e6ce4bdc0f8d58172ed6a6099e2ead48
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-04-05 10:16:26 +0900

    🍎imageSEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,8 @@ const Home = ({ posts }) => {
         pageName: "emoji日記 | 今日の出来事を絵文字で現そう。",
         description:
           "今日の出来事を30秒で振り返りませんか？良い思い出もあれば悪い思い出もある。それで良いのです。",
-        cardImage: "/icon/1024.png",
+        cardImage:
+          "https://user-images.githubusercontent.com/67810971/113526962-e320c080-95f6-11eb-8f5d-22d1aa1ba5da.png",
       }}
     >
       <div className="flex flex-col w-full px-4">
